415ff265c01294435cac423798e88ae6a761c048
cuikho210 <tonykms210@gmail.com>
2024-02-27 06:43:02 +0700

    Update publish script

--- check_to_publish.bash
+++ check_to_publish.bash
@@ -6,8 +6,12 @@ git submodule update
 
 # Check diff in commit id
 latest_remixicon_commit_id=$(git --git-dir ./remixicon/.git log --format="%h" -n 1)
+
 if [ "$latest_remixicon_commit_id" = "$(cat ./latest_remixicon_commit_id)" ]; then
+	echo "No change"
+else
 	build_and_publish
+	update_remixicon_commit_id
 fi
 
 function build_and_publish() {
@@ -20,4 +24,14 @@ function build_and_publish() {
 
 	# Publish
 	yarn --cwd ./publish semantic-release
+}
+
+function update_remixicon_commit_id() {
+	echo "$latest_remixicon_commit_id" > ./latest_remixicon_commit_id
+
+	git config --global user.name cuikho210 
+	git config --global user.email tonykms210@gmail.com 
+	git add -u
+	git commit -m "Github actions: Auto update $(date)"
+	git push
 }
